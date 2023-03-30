707f9dd083e63c392145e5932b159c40b424db9a
ankita <khare16ankita@gmail.com>
2023-03-31 09:30:41 +1300

    DemoApp
    ListView Implemented
    Button , AlertBox, Combo Box implemented

---
+++ DemoApp/DemoApp/DemoApp/LoginUI.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DemoApp
+
+{
+    public partial class LoginUI : ContentPage
+    {
+        public LoginUI()
+        {
+            InitializeComponent();
+        }
+        private void OnLoginButtonClicked(object sender, EventArgs e)
+        {
+            string userEmail = emailEntry.Text;
+            string userPassword = passwordEntry.Text;
+
+            if (string.IsNullOrEmpty(userEmail) || string.IsNullOrEmpty(userPassword))
+            {
+                DisplayAlert("Error", "Please enter email and password", "OK");
+            }
+            else if (userEmail == "ankikrai16@gmail.com" && userPassword == "Ondot@123")
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                DisplayAlert("Error", "Invalid email or password", "OK");
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
